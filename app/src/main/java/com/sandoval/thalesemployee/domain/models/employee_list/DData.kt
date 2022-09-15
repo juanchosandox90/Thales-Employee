7dcaeb410c79e466a8a6af1925cfcc0c661a62a0
Juan Camilo Sandoval Devia <juansandoval222@gmail.com>
2022-09-15 17:35:42 -0500

    Connect Domain With Data
    - Add to the data models classes. method to map to domain models the values
    - Consume the service from the data repository implementation and populate the values through the domain mapping models

--- app/src/main/java/com/sandoval/thalesemployee/domain/models/employee_list/DData.kt
+++ app/src/main/java/com/sandoval/thalesemployee/domain/models/employee_list/DData.kt
@@ -1,9 +1,9 @@
 package com.sandoval.thalesemployee.domain.models.employee_list
 
 data class DData(
-    val employeeAge: Int,
-    val employeeName: String,
-    val employeeSalary: Int,
-    val id: Int,
-    val profileImage: String
+    val employeeAge: Int?,
+    val employeeName: String?,
+    val employeeSalary: Int?,
+    val id: Int?,
+    val profileImage: String?
 )
--- app/src/main/java/com/sandoval/thalesemployee/domain/usecase/employee_list/GetEmployeeListUseCase.kt
+++ app/src/main/java/com/sandoval/thalesemployee/domain/usecase/employee_list/GetEmployeeListUseCase.kt
@@ -1,9 +1,9 @@
-package com.sandoval.thalesemployee.domain.usecase
+package com.sandoval.thalesemployee.domain.usecase.employee_list
 
 import com.sandoval.thalesemployee.data.network.Failure
 import com.sandoval.thalesemployee.data.utils.Either
 import com.sandoval.thalesemployee.domain.models.employee_list.DData
-import com.sandoval.thalesemployee.domain.repository.IGetEmployeeListRepository
+import com.sandoval.thalesemployee.domain.repository.employee_list.IGetEmployeeListRepository
 import com.sandoval.thalesemployee.domain.base.BaseUseCase
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
