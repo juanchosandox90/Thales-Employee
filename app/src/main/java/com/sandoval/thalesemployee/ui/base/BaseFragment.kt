03f2d901f5f0e1b0b07a10d824879b098955e8fa
Juan Camilo Sandoval Devia <juansandoval222@gmail.com>
2022-09-17 01:25:39 -0500

    Final Touches
    - Add more unit test for the viewmodel layer
    - Add network helper to check if the app is online or not
    - Add new icon

--- app/src/main/java/com/sandoval/thalesemployee/ui/base/BaseFragment.kt
+++ app/src/main/java/com/sandoval/thalesemployee/ui/base/BaseFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
+import com.sandoval.thalesemployee.data.network.NetworkHelper
 
 abstract class BaseFragment<DB : ViewDataBinding>(
     private val _bindingInflater: (inflater: LayoutInflater) -> DB
@@ -14,6 +15,8 @@ abstract class BaseFragment<DB : ViewDataBinding>(
     private var _binding: DB? = null
     val binding: DB get() = _binding as DB
 
+    private var networkHelper: NetworkHelper? = null
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -21,12 +24,21 @@ abstract class BaseFragment<DB : ViewDataBinding>(
     ): View? {
 
         _binding = _bindingInflater.invoke(inflater)
-        initViews()
-        initViewModels()
+
+        networkHelper = NetworkHelper(requireContext())
+        when {
+            networkHelper!!.isNetworkConnected() -> {
+                initViews()
+                initViewModels()
+            }
+            else -> showNoInternetConnection()
+        }
+
         when (_binding) {
             null -> throw IllegalArgumentException("Binding cannot be null")
             else -> return binding.root
         }
+
 
     }
 
@@ -37,4 +49,5 @@ abstract class BaseFragment<DB : ViewDataBinding>(
 
     abstract fun initViews()
     abstract fun initViewModels()
+    open fun showNoInternetConnection() {}
 }
